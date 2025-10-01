29fd5db9e36b603588dbbd4d4d200692cee5222e
gyeongjun <kanell0304@gmail.com>
2025-10-01 16:18:56 +0900

    edit image schemas

--- backend/app/database/schemas/image.py
+++ backend/app/database/schemas/image.py
@@ -1,8 +1,7 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 class ImageResponse(BaseModel):
     id: int
     filename: str
 
-    class Config:
-        orm_mode = True  #sqlalchemy orm객체 받아서 변환 가능하게 설정
+    model_config = ConfigDict(from_attributes=True)
