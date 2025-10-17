861e40e0e7be69b48d50de0383bf225f4295c77c
gyeongjun <kanell0304@gmail.com>
2025-10-17 13:28:48 +0900

    survey feature updates

--- backend/app/router/survey.py
+++ backend/app/router/survey.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from typing import List
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from backend.app.core.auth import get_user_id
 from backend.app.database.database import get_db
 from backend.app.database.schemas.surveys import SurveyCreate, SurveyOut
 from backend.app.service.survey_service import SurveyService
@@ -10,7 +12,7 @@ router=APIRouter(prefix="/surveys", tags=["Surveys"])
 
 
 @router.post("", response_model=SurveyOut, status_code=status.HTTP_201_CREATED)
-async def create_surveys(survey:SurveyCreate, user_id:int, db:AsyncSession=Depends(get_db)):
+async def create_surveys(survey:SurveyCreate, user_id: int = Depends(get_user_id), db:AsyncSession=Depends(get_db)):
     new_survey=await SurveyService.create_survey(db, user_id, survey)
     return new_survey
 
@@ -19,7 +21,7 @@ async def get_all_surveys(user_id:int, db:AsyncSession=Depends(get_db)):
     return await SurveyService.list_survey(db, user_id)
 
 @router.get("/{survey_id}", response_model=SurveyOut)
-async def get_my_survey(survey_id:int, user_id:int, db:AsyncSession=Depends(get_db)):
+async def get_my_survey(survey_id:int, user_id: int = Depends(get_user_id), db:AsyncSession=Depends(get_db)):
     my_survey=await SurveyService.get_detailed(db, survey_id, user_id)
     if not my_survey:
         raise HTTPException(status_code=404, detail="Survey Not Found")
